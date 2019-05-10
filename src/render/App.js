173a555791e43f22944e3ffdac6eb43f205d9e33
WozHuang <909695699@qq.com>
2019-05-11 00:24:06 +0800

    adjust.

--- src/render/App.js
+++ src/render/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={style.App}>
+      <header className={style['App-header']}>
+        <img src={logo} className={style['App-logo']} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={style.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React123
         </a>
       </header>
     </div>
